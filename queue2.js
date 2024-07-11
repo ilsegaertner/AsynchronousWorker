26ca9a2684001167032f00e4b3b07434aa82759f
Ilse Gaertner <joachimpruegl@gmail.com>
2024-07-11 16:50:01 +0200

    put workerFunction outside of class, refactor for sample usage

--- queue2.js
+++ queue2.js
@@ -1,26 +1,13 @@
 class Queue {
-  constructor(maxConcurrentTasks) {
+  constructor(workerFunction, maxConcurrentTasks) {
     this.maxConcurrentTasks = maxConcurrentTasks;
     this.taskQueue = [];
     this.runningTasks = 0;
     this.isPaused = false;
-    this.hasError = false;
   }
 
   limitConcurrentTasks() {
     return this.runningTasks < this.maxConcurrentTasks;
-  }
-
-  async workerFunction(task) {
-    const randomTime = Math.random() * 1000;
-    await new Promise((resolve, reject) => {
-      if (!this.hasError) {
-        setTimeout(resolve, randomTime);
-      } else {
-        reject(new Error("task encountered an error"));
-      }
-    });
-    console.log(`${task} waited for ${randomTime}`);
   }
 
   push(task) {
@@ -37,16 +24,12 @@ class Queue {
       return;
     }
     const task = this.taskQueue.shift();
-    this.runningTasks += 1;
-
-    try {
-      await this.workerFunction(task);
-    } catch (error) {
-      console.error(`Error in processing task: ${error.message}`);
-    } finally {
+    this.runningTasks += 1; // shorthand way of writing this.runningTasks = this.runningTasks + 1
+    this.workerFunction(task).then(() => {
+      // processing the task
       this.runningTasks -= 1;
-      this.processTasks();
-    }
+      this.processTasks(); //  ensures that as soon as one task finishes, the next task can start, maintaining a continuous flow of task processing within the constraints.
+    });
   }
 
   pause() {
@@ -64,7 +47,7 @@ class Queue {
         if (this.taskQueue.length === 0 && this.runningTasks === 0) {
           resolve();
         } else {
-          setTimeout(checkIfDone, 100);
+          setTimeout(checkIfDone, 100); // creating a polling mechanism
         }
       };
       checkIfDone();
@@ -72,7 +55,14 @@ class Queue {
   }
 }
 
-const queue = new Queue(3);
+async function main() {
+  // Create a queue for an asynch worker function with 3 concurrent workers
+  const queue = new Queue(async (x) => {
+    const duration = Math.random() * 1000 * x;
+    await new Promise((resolve) => setTimeout(resolve, duration));
+    console.log(`${x} waited for ${duration}ms`);
+  }, 3);
+}
 
 queue.push(2);
 queue.push(3);
@@ -80,11 +70,7 @@ queue.push(4);
 queue.push(5);
 queue.push(6);
 
-queue
-  .waitForAll()
-  .then(() => {
-    console.log("All done");
-  })
-  .catch((error) => {
-    console.error(error.message);
-  });
+await queue.waitForAll();
+console.log("All done");
+
+main();
