36fa3b579c9ffc63be996ed32e28c217293ae6eb
Ilse Gaertner <joachimpruegl@gmail.com>
2024-07-11 17:59:48 +0200

    implement callback

--- queue2.js
+++ queue2.js
@@ -11,8 +11,8 @@ class Queue {
     return this.runningTasks < this.maxConcurrentTasks;
   }
 
-  push(task) {
-    this.taskQueue.push(task);
+  push(task, callback) {
+    this.taskQueue.push({ task, callback });
     this.processTasks();
   }
 
@@ -24,13 +24,19 @@ class Queue {
     ) {
       return;
     }
-    const task = this.taskQueue.shift();
-    this.runningTasks += 1; // shorthand way of writing this.runningTasks = this.runningTasks + 1
-    this.workerFunction(task).then(() => {
-      // processing the task
+    const { task, callback } = this.taskQueue.shift();
+    try {
+      this.runningTasks += 1;
+      await this.workerFunction(task);
+      if (callback) callback();
+    } catch {
+      (error) => {
+        console.error(error.message);
+      };
+    } finally {
       this.runningTasks -= 1;
-      this.processTasks(); //  ensures that as soon as one task finishes, the next task can start, maintaining a continuous flow of task processing within the constraints.
-    });
+      this.processTasks();
+    }
   }
 
   pause() {
@@ -48,7 +54,7 @@ class Queue {
         if (this.taskQueue.length === 0 && this.runningTasks === 0) {
           resolve();
         } else {
-          setTimeout(checkIfDone, 100); // creating a polling mechanism
+          setTimeout(checkIfDone, 100);
         }
       };
       checkIfDone();
@@ -57,7 +63,6 @@ class Queue {
 }
 
 async function main() {
-  // Create a queue for an asynch worker function with 3 concurrent workers
   const queue = new Queue(async (x) => {
     const duration = Math.random() * 1000 * x;
     await new Promise((resolve) => setTimeout(resolve, duration));
@@ -67,7 +72,9 @@ async function main() {
   queue.push(10);
   queue.push(9);
   queue.push(8);
-  queue.push(7);
+  queue.push(7, () => {
+    console.log("this was the beautiful task 7");
+  });
   queue.push(6);
   queue.push(5);
   queue.push(4);
