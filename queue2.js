f089ca41220c76eec2ffc5ad43b6d84eac3e2fc4
Ilse Gaertner <joachimpruegl@gmail.com>
2024-07-13 12:03:12 +0200

    add retry and task class

--- queue2.js
+++ queue2.js
@@ -1,18 +1,32 @@
+class Task {
+  constructor(id, taskData, callback = null, retries = 4) {
+    (this.id = id),
+      (this.taskData = taskData),
+      (this.callback = callback),
+      (this.retries = retries);
+    this.isCancelled = false;
+  }
+}
+
+const task = new Task();
+
 class Queue {
-  constructor(workerFunction, maxConcurrentTasks) {
+  constructor(workerFunction, maxConcurrentTasks, retryLimit) {
     this.workerFunction = workerFunction;
     this.maxConcurrentTasks = maxConcurrentTasks;
     this.taskQueue = [];
     this.runningTasks = 0;
     this.isPaused = false;
+    this.retryLimit = retryLimit;
+    this.cancelQueuedTask = false;
   }
 
   limitConcurrentTasks() {
     return this.runningTasks < this.maxConcurrentTasks;
   }
 
-  push(task, callback) {
-    this.taskQueue.push({ task, callback });
+  push(task) {
+    this.taskQueue.push(task);
     this.processTasks();
   }
 
@@ -24,13 +38,21 @@ class Queue {
     ) {
       return;
     }
-    const { task, callback } = this.taskQueue.shift();
+    const task = this.taskQueue.shift();
     try {
       this.runningTasks += 1;
       await this.workerFunction(task);
-      if (callback) callback();
+      if (task.callback) callback();
     } catch (error) {
-      console.error(error.message);
+      console.error(`Error processing task ${task}: ${error.message}`);
+      if (retries < this.retryLimit) {
+        console.log(
+          `retrying task ${task} (${retries + 1}/${this.retryLimit})`
+        );
+        this.push(task, callback, retries + 1);
+      } else {
+        console.error(`Task ${task} failed after ${this.retryLimit} retries.`);
+      }
     } finally {
       this.runningTasks -= 1;
       this.processTasks();
@@ -44,6 +66,10 @@ class Queue {
   resume() {
     this.isPaused = false;
     this.processTasks();
+  }
+
+  cancelQueuedTask() {
+    if (this.taskQueue) this.cancelQueuedTask = true;
   }
 
   waitForAll() {
@@ -61,11 +87,15 @@ class Queue {
 }
 
 async function main() {
-  const queue = new Queue(async (x) => {
-    const duration = Math.random() * 1000 * x;
-    await new Promise((resolve) => setTimeout(resolve, duration));
-    console.log(`${x} waited for ${duration}ms`);
-  }, 3);
+  const queue = new Queue(
+    async (x) => {
+      const duration = Math.random() * 1000 * x;
+      await new Promise((resolve) => setTimeout(resolve, duration));
+      console.log(`${x} waited for ${duration}ms`);
+    },
+    3,
+    4
+  );
 
   queue.push(10);
   queue.push(9);
