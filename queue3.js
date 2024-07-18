13bfb378678e585014a31bb7ff9eb33e26f26dcd
Ilse Gaertner <joachimpruegl@gmail.com>
2024-07-18 11:11:35 +0200

    create solution2-priority

--- queue3.js
+++ queue3.js
@@ -1,6 +1,6 @@
 class Queue {
-  constructor(workerFunction, maxConcurrentTasks) {
-    this.workerFunction = workerFunction;
+  constructor(workerFn, maxConcurrentTasks) {
+    this.workerFn = workerFn;
     this.maxConcurrentTasks = maxConcurrentTasks;
     this.taskQueue = [];
     this.runningTasks = 0;
@@ -11,8 +11,9 @@ class Queue {
     return this.runningTasks < this.maxConcurrentTasks;
   }
 
-  push(task, callback) {
-    this.taskQueue.push({ task, callback });
+  push(task, callback, priority = 1) {
+    this.taskQueue.push({ task, callback, priority });
+    this.taskQueue.sort((a, b) => b.priority - a.priority);
     this.processTasks();
   }
 
@@ -24,15 +25,16 @@ class Queue {
     ) {
       return;
     }
-    const { task, callback } = this.taskQueue.shift();
+    const { task, callback, priority } = this.taskQueue.shift();
     try {
       this.runningTasks += 1;
-      await this.workerFunction(task);
+      await this.workerFn(task);
       if (callback) callback(task);
     } catch (error) {
       console.error(error.message);
     } finally {
       this.runningTasks -= 1;
+      this.taskQueue.sort((a, b) => b.priority - a.priority);
       this.processTasks();
     }
   }
@@ -63,8 +65,8 @@ class Queue {
 async function main() {
   const queue = new Queue(async (x) => {
     const duration = Math.random() * 1000 * x;
-    await new Promise((resolve) => setTimeout(resolve, duration));
-    console.log(`${x} waited for ${duration}ms`);
+    await new Promise((r) => setTimeout(r, duration));
+    console.log(`${x} waited for ${duration} ms`);
   }, 3);
 
   queue.push(10);
@@ -73,15 +75,27 @@ async function main() {
   queue.push(7, () => {
     console.log("this was task 7");
   });
-  queue.push(6, null);
-  queue.push(5, null);
-  queue.push(4, null);
+  queue.push(6);
+  queue.push(
+    5,
+    () => {
+      console.log("This is task 5");
+    },
+    9
+  );
+  queue.push(
+    4,
+    () => {
+      console.log("This is task 4");
+    },
+    3
+  );
   queue.push(3);
   queue.push(2);
   queue.push(1);
 
   await queue.waitForAll();
-  console.log("All done");
+  console.log("All done!");
 }
 
 main();
